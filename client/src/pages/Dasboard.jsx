8f50d39aca01f1ec07f8a3b0da9e6eb673f0a33a
Avinash-1-10 <avinashyede110@gmail.com>
2024-06-03 19:44:56 +0530

    add barchart

--- client/src/pages/Dasboard.jsx
+++ client/src/pages/Dasboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LineGraph from "../components/dashboard/LineGraph";
 import UserCard from "../components/dashboard/UserCard";
+import BarChart from "../components/dashboard/BarChart";
 
 const Dasboard = () => {
   return (
     <div>
       <UserCard />
       <LineGraph />
+      <BarChart/>
     </div>
   );
 };
