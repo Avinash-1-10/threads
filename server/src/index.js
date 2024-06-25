420bb6e06f8028b0f6c99a97d351ed372b8f2c40
Avinash-1-10 <avinashyede110@gmail.com>
2024-06-25 18:20:24 +0530

    add custom cors middleware

--- server/src/index.js
+++ server/src/index.js
@@ -26,7 +26,18 @@ const PORT = process.env.PORT || 4000;
 setupPassport(app);
 
 // middlewares
-app.use(cors());
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
